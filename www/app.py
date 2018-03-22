d16088eda5c8137ce78ebdd5b02deb6553cf285f
ken <3297370485@qq.com>
2018-03-22 16:49:05 +0800

    编辑index 欢迎信息

--- www/app.py
+++ www/app.py
@@ -21,7 +21,7 @@ from datetime import datetime
 from aiohttp import web
 
 def index(request):
-    return web.Response(body=b'<h1>Awesome</h1>',content_type='text/html')
+    return web.Response(body=b'<h1>Welcome Ken`s blog!</h1>',content_type='text/html')
 
 async def init(loop):
     app = web.Application(loop=loop)
