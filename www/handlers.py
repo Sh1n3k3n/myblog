431f7e805e07388670928b7671604af999c46313
ken <3297370485@qq.com>
2018-03-29 14:25:31 +0800

    day7 实现MVC

--- www/handlers.py
+++ www/handlers.py
@@ -17,14 +17,21 @@ __author__ = 'Michael Liao'
 
 ' url handlers '
 
-from www.coroweb import get
+from www.coroweb import get, post
 
-import asyncio
+import asyncio, re, time, json, logging, hashlib, base64
+
+from www.models import User, Comment, Blog, next_id
+
 
 'example'
 @get('/')
 async def index(request):
-    return '<h1>Awesome</h1>'
+    users = await User.findAll()
+    return {
+        '__template__': 'test.html',
+        'users': users
+    }
 
 @get('/hello')
 async def hello(request):
