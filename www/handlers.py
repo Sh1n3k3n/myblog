363ab73d91efc32b0abdad3eec72a2ab64ab6b8b
ken <3297370485@qq.com>
2018-03-30 10:38:00 +0800

    day8 构建前端

--- www/handlers.py
+++ www/handlers.py
@@ -23,7 +23,7 @@ import asyncio, re, time, json, logging, hashlib, base64
 
 from www.models import User, Comment, Blog, next_id
 
-
+'''
 'example'
 @get('/')
 async def index(request):
@@ -32,8 +32,26 @@ async def index(request):
         '__template__': 'test.html',
         'users': users
     }
+'''
 
-@get('/hello')
-async def hello(request):
-    return '<h1>Hello World!</h1>'
+# =============================================
+#
+#            重新定义了index的内容
+#
+# =============================================
+#  summary及blogs会被blogs.html 通过jinja2模块抽取出来填充至html展示
+#  app的datetime_filter 就是为了处理这里的时间让jinja2加载的过滤器
+# =============================================
+@get('/')
+def index(request):
+    summary = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    blogs = [
+        Blog(id='1', name='Test Blog', summary=summary, created_at=time.time()-120),
+        Blog(id='2', name='Something New', summary=summary, created_at=time.time()-3600),
+        Blog(id='3', name='Learn Swift', summary=summary, created_at=time.time()-7200)
+    ]
+    return {
+        '__template__': 'blogs.html',
+        'blogs': blogs
+    }
 
